b450b17688f99db26d81f245ef920bc619737642
Shams-02 <shams@viralsquare.org>
2023-03-29 21:20:20 +0500

    image upload

--- controllers/User.js
+++ controllers/User.js
@@ -1,42 +1,26 @@
-const fs = require('fs');
-
-const AWS = require("aws-sdk");
-require("dotenv").config();
-
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
-
-const uploadImageToS3 = (filename, fileContent) => {
-  const params = {
-    Bucket: "cyclic-wild-erin-cape-buffalo-slip-eu-west-2",
-    Key: filename,
-    Body: fileContent
-  };
-  return s3.upload(params).promise();
-}
-
 exports.patchEditPorfile = async (req, res, next) => {
   try {
-    if (!req.file) {
-      return res.status(400).send("Please upload a file");
-    }
+    // if (!req.file) {
+    //   return res.status(400).send("Please upload a file");
+    // }
 
-    const filename = req.file.filename;
-    const fileContent = fs.readFileSync(req.file.path);
+    // const filename = req.file.filename;
+    // const fileContent = fs.readFileSync(req.file.path);
 
-    const imageUrl = `${req.protocol}://${req.hostname}:3000/${req.file.path}`;
+    // const imageUrl = `${req.protocol}://${req.hostname}:3000/${req.file.path}`;
 
-    uploadImageToS3(filename, fileContent)
-      .then(data => {
-        console.log(`File uploaded successfully. File URL: ${data.Location}`);
-        res.status(200).json({ message: `File uploaded successfully. URL: ${data.Location}` });
+    // uploadImageToS3(filename, fileContent)
+    //   .then(data => {
+    //     console.log(`File uploaded successfully. File URL: ${data.Location}`);
+    //     res.status(200).json({ message: `File uploaded successfully. URL: ${data.Location}` });
 
-      })
-      .catch(error => {
-        console.log(`File upload failed: ${error}`);
-      });
+    //   })
+    //   .catch(error => {
+    //     console.log(`File upload failed: ${error}`);
+    //   });
+    console.log("Hello");
+    const fileUrl = req.file.location;
+    res.status(200).json({ message: fileUrl })
 
   } catch (error) {
     console.log(error);
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,22 +1,173 @@
-const multer = require('multer');
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const { S3Client } = require("@aws-sdk/client-s3");
+const path = require("path");
+require("dotenv").config();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+// create s3 instance using S3Client 
+// (this is how we create s3 instance in v3)
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+  region: process.env.AWS_REGION
+})
+
+
+const s3Storage = multerS3({
+  s3: s3, // s3 instance
+  bucket: "my-images", // change it as per your project requirement
+  acl: "public-read", // storage access type
+  metadata: (req, file, cb) => {
+    cb(null, { fieldname: file.fieldname })
+  },
+  key: (req, file, cb) => {
+    const fileName = Date.now() + "_" + file.fieldname + "_" + file.originalname;
+    cb(null, fileName);
   }
 });
 
-const upload = multer({ storage: storage });
 
+// function to sanitize files and send error for unsupported files
+function sanitizeFile(file, cb) {
+  // Define the allowed extension
+  const fileExts = [".png", ".jpg", ".jpeg", ".gif"];
 
+  // Check allowed extensions
+  const isAllowedExt = fileExts.includes(
+    path.extname(file.originalname.toLowerCase())
+  );
 
+  // Mime type must be an image
+  const isAllowedMimeType = file.mimetype.startsWith("image/");
 
+  if (isAllowedExt && isAllowedMimeType) {
+    return cb(null, true); // no errors
+  } else {
+    // pass error msg to callback, which can be displaye in frontend
+    cb("Error: File type not allowed!");
+  }
+}
 
+// our middleware
+const upload = multer({
+  storage: s3Storage,
+  fileFilter: (req, file, callback) => {
+    sanitizeFile(file, callback)
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 2 // 2mb file size
+  }
+})
 
 module.exports = upload;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require("dotenv").config();
+// const AWS = require('aws-sdk');
+// const multerS3 = require('multer-s3');
+// const multer = require("multer");
+
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.AWS_REGION
+// });
+// const s3 = new AWS.S3();
+
+// const upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     bucket: 'cyclic-wild-erin-cape-buffalo-slip-eu-west-2',
+//     acl: 'public-read',
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     metadata: function (req, file, cb) {
+//       cb(null, { fieldName: file.fieldname });
+//     },
+//     key: function (req, file, cb) {
+//       cb(null, Date.now().toString())
+//     }
+//   })
+// });
+
+
+
+
+// module.exports = upload;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const multer = require('multer');
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/');
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + '-' + file.originalname);
+//   }
+// });
+
+//   const upload = multer({ storage: storage });
+
+
+
+
+
+
 
 
 
